5dbf6b2b457e8822cc62a3510a462bbf959f893a
cristianecorreia <cristianealive@gmail.com>
2022-11-13 15:29:56 -0300

    criado validaçao dos campos da tela de cadastro de login

--- app/src/main/java/com/example/appsenai/MainActivity.java
+++ app/src/main/java/com/example/appsenai/MainActivity.java
@@ -9,15 +9,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button botao;
+    private TextView txt_tela_cadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportActionBar().hide();
+
+        txt_tela_cadastro = findViewById(R.id.txtTelaCadastro);
+        txt_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
 
 
         botao = findViewById(R.id.btEntrar);
@@ -30,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 }
