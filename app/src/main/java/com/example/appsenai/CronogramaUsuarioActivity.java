9c654e2e6023c5c0570583bed46e5814d8022fa1
Machado, Gabriel <gabrielgamerlive@gmail.com>
2022-11-22 22:08:18 -0300

    alterações para teste com gravação em database firebase

---
+++ app/src/main/java/com/example/appsenai/CronogramaUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsenai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CronogramaUsuarioActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cronograma_usuario);
+
+    }
+}
